b403006d5bc36d95870bf9e4eb801c7322e9b43f
Trentan Jurkans <107039183+TRN10@users.noreply.github.com>
2022-10-16 21:39:37 +1100

    index/notes.js routes and folder created

---
+++ routes/notes.js
@@ -0,0 +1,32 @@
+const tips = require('express').Router();
+const { readFromFile, readAndAppend } = require('../helpers/fsUtils');
+const { v4: uuidv4 } = require('uuid');
+
+// GET route for retrieving saved notes
+
+notes.get('/', (req, res) => {
+    readFromFile('./db.json').then((data) => res.json(JSON.parse(data)));
+});
+
+
+// POST Route for saving note
+
+notes.post('/', (req, res) => {
+    console.log(req.body);
+
+    const { text } = req.body;
+
+    if (req.body) {
+        const newNote = {
+            text,
+            note_id: uuidv4(),
+        };
+
+        readAndAppend(newNote, './db.json');
+        res.json(`note added successfully 🚀`);
+    } else {
+        res.error('Error in adding note');
+    }
+});
+
+module.exports = notes;
